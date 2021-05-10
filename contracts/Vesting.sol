2f31061f0a8822ede5cfc2f6fbbd9bdeddb7cecb
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-05-10 14:48:40 +1000

    Feat: Add hard hat tests

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+struct Deposit {
+    uint256 amount;
+    uint256 date;
+}
 
 contract Vesting is Ownable {
 
@@ -11,6 +16,7 @@ contract Vesting is Ownable {
 
     address private immutable _token;
     mapping (address => uint256) private _balances;
+    mapping (address => Deposit) private _deposits;
 
     constructor(address token) public {
         require(token != address(0), "Invalid address");
@@ -20,7 +26,9 @@ contract Vesting is Ownable {
     function deposit(uint256 amount) public {
         require(amount > 0, "Doesn't make sense");
         IERC20(_token).transferFrom(address(msg.sender), address(this), amount);
-        _balances[msg.sender] = _balances[msg.sender].add(amount);
+        // _balances[msg.sender] = _balances[msg.sender].add(amount);
+
+        _deposits[msg.sender] = Deposit(amount, block.timestamp);
 
         emit DepositReceived(amount, msg.sender);
     }
--- test/grants.test.js
+++ test/grants.test.js
@@ -16,7 +16,7 @@
 
 //   beforeEach(async () => {
 //     const token = await Token.new();
-//     grants = await Grants.new(token.address, "0x34ac70849AF62a97036b73BcF5A49e17B29Ba19B");
+//     grants = await Grants.new(token.address, "");
 //   });
 
 //   describe("properties", () => {
@@ -28,3 +28,27 @@
 //     });
 //   });
 // });
+
+const { expect } = require("chai");
+
+const foundation = "0x34ac70849AF62a97036b73BcF5A49e17B29Ba19B";
+
+describe("Grants", () => {
+
+  let grants;
+
+  beforeEach(async() => {
+    const Token = await ethers.getContractFactory("Token");
+    const token = await Token.deploy();
+    await token.deployed();
+
+    const Grants = await ethers.getContractFactory("Grants");
+    grants = await Grants.deploy(token.address, foundation);
+    await grants.deployed();
+  })
+
+  it("Should return total supply once deployed", async () => {
+    const expected = ethers.BigNumber.from("0");
+    expect(await grants.grantsCount()).to.equal(expected);
+  });
+});
