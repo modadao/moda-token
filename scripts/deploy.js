f4ff6e8d9c24b19773c4544c940fa3fc3d253c27
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-06-08 10:44:04 +1000

    Chore: Fix import

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,4 @@
 const hre = require("hardhat");
-const fs = require("fs");
-const sqlite3 = require("sqlite3").verbose();
-
-// OPEN_CREATE
-// const db = new sqlite3.Database("./db/deploys.db", sqlite3.OPEN_READWRITE);
-const db = new sqlite3.Database("deployments.db", sqlite3.OPEN_CREATE);
 
 const deploy = async () => {
   const [deployer] = await ethers.getSigners();
@@ -14,26 +8,6 @@ const deploy = async () => {
   const token = await Token.deploy();
 
   console.log(`Token deployed to: ${token.address}`);
-
-  const rawdata = fs.readFileSync("emanate.json");
-  const accounts = JSON.parse(rawdata);
-
-  for (const account of accounts) {
-    const sql = `SELECT tx FROM Transactions WHERE Account = ?`;
-
-    db.serialize(() => {
-      db.get(sql, [account.address], (err, row) => {
-        if (err) {
-          console.error(err.message);
-        }
-        // console.log(row.id + "\t" + row.name);
-        console.log(row.id);
-      });
-    });
-    // const tx = await token.mint(account.address, accounts.amount);
-    // console.log(account);
-  }
 };
 
 deploy();
-db.close();
---
+++ scripts/deploy_and_mint.js
@@ -0,0 +1,39 @@
+const hh = require("hardhat");
+const fs = require("fs");
+const sqlite3 = require("sqlite3").verbose();
+
+// OPEN_CREATE
+// const db = new sqlite3.Database("./db/deploys.db", sqlite3.OPEN_READWRITE);
+const db = new sqlite3.Database("deployments.db", sqlite3.OPEN_CREATE);
+
+const deploy = async () => {
+  const [deployer] = await hh.ethers.getSigners();
+
+  // We get the contract to deploy
+  const Token = await hh.ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+
+  console.log(`Token deployed to: ${token.address}`);
+
+  const rawdata = fs.readFileSync("emanate.json");
+  const accounts = JSON.parse(rawdata);
+
+  for (const account of accounts) {
+    const sql = `SELECT tx FROM Transactions WHERE Account = ?`;
+
+    db.serialize(() => {
+      db.get(sql, [account.address], (err, row) => {
+        if (err) {
+          console.error(err.message);
+        }
+        // console.log(row.id + "\t" + row.name);
+        console.log(row.id);
+      });
+    });
+    // const tx = await token.mint(account.address, accounts.amount);
+    // console.log(account);
+  }
+};
+
+deploy();
+db.close();
