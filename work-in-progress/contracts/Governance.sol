46958a1c3373fa7945e1f56d5d71124871f160b6
dexblack <dex@dexblack.net>
2021-09-10 21:57:26 +1000

    Pretty printed uint->uint256 automatic.

--- work-in-progress/contracts/Governance.sol
+++ work-in-progress/contracts/Governance.sol
@@ -1,92 +1,113 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.6;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./ICount.sol";
-import "./Members.sol";
-
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/math/SafeMath.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import './ICount.sol';
+import './Members.sol';
 
 struct Proposal {
-    string issue;
-    address owner;
-    uint256 expires;
-    uint256 accept;
-    uint256 reject;
-    uint min;
-    uint max;
+	string issue;
+	address owner;
+	uint256 expires;
+	uint256 accept;
+	uint256 reject;
+	uint256 min;
+	uint256 max;
 }
 
 contract Governance is ICount, Ownable {
-    using SafeMath for uint;
+	using SafeMath for uint256;
 
-    mapping (uint256 => mapping(address => bool)) public votes;
-    Proposal[] public proposals;
+	mapping(uint256 => mapping(address => bool)) public votes;
+	Proposal[] public proposals;
 
-    IERC20 immutable private erc20; // MODA DAO Token
-    address immutable private _foundation;
-    Members immutable private _members;
+	IERC20 private immutable erc20; // MODA DAO Token
+	address private immutable _foundation;
+	Members private immutable _members;
 
-    function Count() public override view returns (uint256) {
-        return proposals.length;
-    }
+	function Count() public view override returns (uint256) {
+		return proposals.length;
+	}
 
-    constructor(address token, address foundation, address members) {
-        require(token != address(0) && foundation != address(0), "Invalid address");
-        erc20 = IERC20(token);
-        _foundation = foundation;
-        _members = Members(members);
-    }
+	constructor(
+		address token,
+		address foundation,
+		address members
+	) {
+		require(token != address(0), 'Invalid token address');
+		require(foundation != address(0), 'Invalid foundation address');
+		require(token != foundation, 'Addresses for token and foundation cannot be the same');
 
-    function inVotingPeriod(uint index) public view returns (bool) {
-        return proposals[index].expires > block.timestamp;
-    }
+		erc20 = IERC20(token);
+		_foundation = foundation;
+		_members = Members(members);
+	}
 
-    function addProposal(string memory issue) public returns(uint256) {
-        require(_members.isMember(msg.sender) == true, "Only members can add proposals");
-        require(erc20.balanceOf(msg.sender) >= 1, "Need at least one token");
+	function inVotingPeriod(uint256 index) public view returns (bool) {
+		return proposals[index].expires > block.timestamp;
+	}
 
-        erc20.transferFrom(msg.sender, address(this), 1);
-        Proposal memory proposal = Proposal(issue, msg.sender, block.timestamp + 48 hours, 0, 0, 1, 1);
-        proposals.push(proposal);
+	function addProposal(string memory issue) public returns (uint256) {
+		require(_members.isMember(msg.sender) == true, 'Only members can add proposals');
+		require(erc20.balanceOf(msg.sender) >= 1, 'Need at least one token');
 
-        return proposals.length;
-    }
+		erc20.transferFrom(msg.sender, address(this), 1);
+		Proposal memory proposal = Proposal(
+			issue,
+			msg.sender,
+			block.timestamp + 48 hours,
+			0,
+			0,
+			1,
+			1
+		);
+		proposals.push(proposal);
 
-    function _vote(uint256 index, bool accept, uint256 amount) private {
-        require(proposals[index].owner != msg.sender, "Cannot vote on own proposal");
-        require(votes[index][msg.sender] != true, "Cannot vote twice");
-        
-        require(inVotingPeriod(index), "Proposal closed for voting");
-        require(amount >= proposals[index].min && proposals[index].max >= amount, "Amount outside the bounds.");
+		return proposals.length;
+	}
 
-        votes[index][msg.sender] = true;
-        erc20.transferFrom(msg.sender, address(this), amount);
+	function _vote(
+		uint256 index,
+		bool accept,
+		uint256 amount
+	) private {
+		require(proposals[index].owner != msg.sender, 'Cannot vote on own proposal');
+		require(votes[index][msg.sender] != true, 'Cannot vote twice');
 
-        if (accept) {
-            proposals[index].accept = proposals[index].accept.add(amount);
-        } else {
-            proposals[index].reject = proposals[index].accept.add(amount);
-        }
-    }
+		require(inVotingPeriod(index), 'Proposal closed for voting');
+		require(
+			amount >= proposals[index].min && proposals[index].max >= amount,
+			'Amount outside the bounds.'
+		);
 
-    function acceptProposal(uint256 index) public {
-        require(_members.isMember(msg.sender) == true, "Only members can vote");
-        require(erc20.balanceOf(msg.sender) >= 1, "Need more tokens");
-        _vote(index, true, 1);
+		votes[index][msg.sender] = true;
+		erc20.transferFrom(msg.sender, address(this), amount);
 
-        emit Accepted(index, msg.sender);
-    }
+		if (accept) {
+			proposals[index].accept = proposals[index].accept.add(amount);
+		} else {
+			proposals[index].reject = proposals[index].accept.add(amount);
+		}
+	}
 
-    function rejectProposal(uint256 index) public {
-        require(_members.isMember(msg.sender) == true, "Only members can vote");
-        require(erc20.balanceOf(msg.sender) >= 1, "Need more tokens");
-        _vote(index, false, 1);
+	function acceptProposal(uint256 index) public {
+		require(_members.isMember(msg.sender) == true, 'Only members can vote');
+		require(erc20.balanceOf(msg.sender) >= 1, 'Need more tokens');
+		_vote(index, true, 1);
 
-        emit Rejected(index, msg.sender);
-    }
+		emit Accepted(index, msg.sender);
+	}
 
-    event Accepted(uint256 index, address indexed who);
-    event Rejected(uint256 index, address indexed who);
+	function rejectProposal(uint256 index) public {
+		require(_members.isMember(msg.sender) == true, 'Only members can vote');
+		require(erc20.balanceOf(msg.sender) >= 1, 'Need more tokens');
+		_vote(index, false, 1);
+
+		emit Rejected(index, msg.sender);
+	}
+
+	event Accepted(uint256 index, address indexed who);
+	event Rejected(uint256 index, address indexed who);
 }
