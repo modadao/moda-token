68f32a42f0b147edc91356c3972a90c20a43ef56
dexblack <dex@dexblack.net>
2021-09-18 17:32:11 +1000

    No coverage required for interfaces.

--- .solcover.js
+++ .solcover.js
@@ -2,6 +2,8 @@ module.exports = {
     skipFiles: [
         'UpgradeTestToken.sol',
         'IVestingToken.sol',
+        'ILinkedToMODA.sol',
+        'ICorePool.sol',
         'IPool.sol',
         'ModaConstants.sol'
     ]
