fddadb28d72ff5083b9b965b9a59c2ae912ff9b2
Dylan Thorne <dylan.thorne@exogee.com>
2022-04-13 14:40:13 +1000

    refactor test setup

---
+++ test/rewards.test.ts
@@ -0,0 +1,160 @@
+import { parseEther } from '@ethersproject/units';
+import chai, { expect } from 'chai';
+import chaiDateTime from 'chai-datetime';
+import { add, fastForward, toTimestampBN } from './utils';
+import { Setup, setup } from './setup';
+import { Token } from '../typechain-types';
+import { upgrades, ethers } from 'hardhat';
+import { BigNumber } from 'ethers';
+import { takeSnapshot, revertSnapshot } from './helper';
+
+chai.use(chaiDateTime);
+
+describe('Rewards', () => {
+	let data: Setup;
+	let snapshotId = 0;
+	beforeEach(async () => {
+		snapshotId = await takeSnapshot();
+		data = await setup();
+		return data;
+	});
+	afterEach(async () => revertSnapshot(snapshotId));
+
+	it('Should have less than the allowed moda total rewards after 2 years', async () => {
+		const { start, firstUser, secondUser, thirdUser, modaCorePool, lpPool, moda } = data;
+
+		const eth = parseEther('1');
+		const amount1 = parseEther('100');
+		const stakeAmount = parseEther('1');
+
+		const tokenFactory = await ethers.getContractFactory('Token');
+		const token = (await upgrades.deployProxy(
+			tokenFactory,
+			[
+				[firstUser.address, secondUser.address, thirdUser.address],
+				[amount1, amount1, amount1],
+			],
+			{
+				kind: 'uups',
+			}
+		)) as Token;
+		await token.deployed();
+
+		await token.connect(firstUser).approve(modaCorePool.address, amount1);
+
+		const lockUntil1 = toTimestampBN(add(start, { years: 1 }));
+		await modaCorePool.connect(firstUser).stake(stakeAmount, lockUntil1);
+
+		const futureDate1: Date = add(start, { years: 1 });
+		await fastForward(futureDate1);
+
+		expect(await modaCorePool.getDepositsLength(firstUser.address)).to.eq(
+			1,
+			'Should have rewards deposited'
+		);
+		const year1tokens = await token.allowance(firstUser.address, modaCorePool.address);
+		expect(year1tokens.div(eth)).to.eq(100, 'Should have token allowance = initial deposit');
+
+		const lockUntil2 = toTimestampBN(add(futureDate1, { years: 1 }));
+		await modaCorePool.connect(firstUser).stake(stakeAmount, lockUntil2);
+
+		const futureDate2: Date = add(futureDate1, { years: 1 });
+		await fastForward(futureDate2);
+
+		await modaCorePool.connect(firstUser).processRewards();
+
+		const deposits = await modaCorePool.getDepositsLength(firstUser.address);
+		expect(deposits).to.eq(4);
+
+		let totalRewards = BigNumber.from(0);
+		for (let i = 0; i < deposits.toNumber(); i++) {
+			const deposit = await modaCorePool.getDeposit(firstUser.address, i);
+			const tokens = deposit.tokenAmount.div(eth);
+			totalRewards = totalRewards.add(deposit.tokenAmount);
+		}
+
+		const allowedRewards = BigNumber.from(202428);
+		expect(totalRewards.div(eth).lt(allowedRewards)).to.be.true;
+	});
+
+	it('Should have correct rewards with multiple pools', async () => {
+		const { start, firstUser, secondUser, modaCorePool, lpPool } = data;
+		const userStakeAmount = parseEther('10');
+		const lockUntil = toTimestampBN(add(start, { years: 1 }));
+		await modaCorePool.connect(firstUser).stake(userStakeAmount, lockUntil);
+		await lpPool.connect(firstUser).stake(userStakeAmount, lockUntil);
+
+		const thirtyDaysAfter = add(start, { days: 30 });
+		await fastForward(thirtyDaysAfter);
+
+		const modaPoolRewardsAfter30Days = await modaCorePool.pendingYieldRewards(firstUser.address);
+		expect(modaPoolRewardsAfter30Days).to.be.gt(0);
+
+		expect(
+			await lpPool.pendingYieldRewards(firstUser.address),
+			'LP pool rewards for first user after 30 days'
+		).to.eq(modaPoolRewardsAfter30Days.mul(2));
+
+		expect(await modaCorePool.getDepositsLength(firstUser.address)).to.eq(1);
+		expect(await lpPool.getDepositsLength(firstUser.address)).to.eq(1);
+		await modaCorePool.connect(firstUser).processRewards();
+		expect(
+			await modaCorePool.getDepositsLength(firstUser.address),
+			'Has original deposit and a yield'
+		).to.eq(2);
+		await lpPool.connect(firstUser).processRewards();
+		expect(
+			await modaCorePool.getDepositsLength(firstUser.address),
+			'LP Pool rewards paid out in MODA Pool yield deposits'
+		).to.eq(4);
+
+		const rewardsAfterClaim = await modaCorePool.pendingYieldRewards(firstUser.address);
+		expect(rewardsAfterClaim).to.equal(0);
+
+		await modaCorePool.connect(secondUser).stake(userStakeAmount, lockUntil);
+
+		const sixtyDaysAfter = add(start, { days: 60 });
+		await fastForward(sixtyDaysAfter);
+
+		const firstUserRewards = await modaCorePool.pendingYieldRewards(firstUser.address);
+		const secondUserRewards = await modaCorePool.pendingYieldRewards(secondUser.address);
+
+		// TODO: calculate these values (not hard code) and check
+		// expect(firstUserRewards, 'First user').to.equal(BigNumber.from('4165215393737488061985121'));
+		// expect(secondUserRewards, 'Second user').to.equal(BigNumber.from('6397136248225413888'));
+	});
+
+	it('Resets the pending rewards amount after a user compounds their rewards', async () => {
+		const { modaCorePool, lpPool, start, firstUser } = data;
+		const firstUserStakeAmount = parseEther('10');
+		const lockUntil = toTimestampBN(add(start, { years: 1 }));
+		await modaCorePool.connect(firstUser).stake(firstUserStakeAmount, lockUntil);
+		await lpPool.connect(firstUser).stake(firstUserStakeAmount, lockUntil);
+
+		const thirtyDaysAfter = add(start, { days: 30 });
+		await fastForward(thirtyDaysAfter);
+
+		expect(
+			await modaCorePool.pendingYieldRewards(firstUser.address),
+			'MODA Core Pool has pending rewards'
+		).to.be.gt(0);
+
+		expect(
+			await lpPool.pendingYieldRewards(firstUser.address),
+			'LP Pool has pending rewards'
+		).to.be.gt(0);
+
+		await modaCorePool.connect(firstUser).processRewards();
+		await lpPool.connect(firstUser).processRewards();
+
+		expect(
+			await modaCorePool.pendingYieldRewards(firstUser.address),
+			'MODA Core Pool has zero pending rewards after compound'
+		).to.eq(0);
+
+		expect(
+			await lpPool.pendingYieldRewards(firstUser.address),
+			'LP Pool has zero pending rewards after compound'
+		).to.eq('0');
+	});
+});
---
+++ test/setup.ts
@@ -0,0 +1,113 @@
+import { ModaCorePool, ModaPoolFactory, TestERC20, Token } from '../typechain-types';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import {
+	addTimestamp,
+	blockNow,
+	fromTimestamp,
+	ROLE_TOKEN_CREATOR,
+	THIRTY_DAYS_IN_SECONDS,
+} from './utils';
+import { ethers, upgrades } from 'hardhat';
+import { parseEther } from '@ethersproject/units';
+import { ContractFactory } from 'ethers';
+
+export type Setup = {
+	factory: ModaPoolFactory;
+	firstUser: SignerWithAddress;
+	lpPool: ModaCorePool;
+	lpToken: TestERC20;
+	moda: Token;
+	modaCorePool: ModaCorePool;
+	owner: SignerWithAddress;
+	secondUser: SignerWithAddress;
+	start: Date;
+	thirdUser: SignerWithAddress;
+	tokenFactory: ContractFactory;
+	modaPoolFactory: ContractFactory;
+	corePoolFactory: ContractFactory;
+};
+
+const MODA_POOL_WEIGHT = 200;
+const LP_POOL_WEIGHT = 400;
+
+export const setup = async (): Promise<Setup> => {
+	const [owner, firstUser, secondUser, thirdUser] = await ethers.getSigners();
+	const start = await blockNow();
+
+	const nextTimestamp = start.getTime() / 1000 + 15;
+	// const userBalance = parseEther('200');
+
+	const tokenFactory = await ethers.getContractFactory('Token');
+	const moda = (await upgrades.deployProxy(
+		tokenFactory,
+		[
+			[firstUser.address, secondUser.address],
+			[parseEther('2000'), parseEther('200')],
+		],
+		{
+			kind: 'uups',
+		}
+	)) as Token;
+	await moda.deployed();
+
+	const modaPerSecond = parseEther('0.02');
+
+	const modaPoolFactory = await ethers.getContractFactory('ModaPoolFactory');
+	const factory = (await modaPoolFactory.deploy(
+		moda.address,
+		modaPerSecond,
+		THIRTY_DAYS_IN_SECONDS,
+		nextTimestamp,
+		addTimestamp(fromTimestamp(nextTimestamp), { years: 2 })
+	)) as ModaPoolFactory;
+	await factory.deployed();
+
+	await factory.createCorePool(nextTimestamp, MODA_POOL_WEIGHT);
+
+	const corePoolFactory = await ethers.getContractFactory('ModaCorePool');
+	const modaCorePool = corePoolFactory.attach(
+		await factory.getPoolAddress(moda.address)
+	) as ModaCorePool;
+
+	const lpTokenFactory = await ethers.getContractFactory('TestERC20');
+	const lpToken = (await lpTokenFactory.deploy(
+		'Sushi LP',
+		'SLP',
+		parseEther('1000000')
+	)) as TestERC20;
+	await lpToken.setBalance(firstUser.address, parseEther('2000'));
+	await lpToken.setBalance(secondUser.address, parseEther('200'));
+
+	const lpPool = (await corePoolFactory.deploy(
+		moda.address,
+		factory.address,
+		modaCorePool.address,
+		lpToken.address,
+		LP_POOL_WEIGHT,
+		nextTimestamp
+	)) as ModaCorePool;
+
+	factory.registerPool(lpPool.address);
+
+	await moda.grantRole(ROLE_TOKEN_CREATOR, factory.address);
+	await moda.connect(firstUser).approve(modaCorePool.address, parseEther('2000'));
+	await moda.connect(secondUser).approve(modaCorePool.address, parseEther('200'));
+	await lpToken.connect(firstUser).approve(lpPool.address, parseEther('2000'));
+	await lpToken.connect(secondUser).approve(lpPool.address, parseEther('200'));
+
+	return {
+		factory,
+		firstUser,
+		lpPool,
+		lpToken,
+		moda,
+		modaCorePool,
+		owner,
+		secondUser,
+		start,
+		thirdUser,
+		tokenFactory,
+		modaPoolFactory,
+		corePoolFactory,
+	};
+};
