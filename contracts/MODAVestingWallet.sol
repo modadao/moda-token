630a9116a494a7ea13bc0bf0abc7e72ecae12ed5
Sam Ruberti <samruberti@gmail.com>
2022-08-16 13:29:17 -0400

    Add multi-sig address and ownable to vesting wallet

--- contracts/MODAVestingWallet.sol
+++ contracts/MODAVestingWallet.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
 
-// import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/finance/VestingWallet.sol';
 
-contract MODAVestingWallet is VestingWallet {
-	constructor(
-		address beneficiary,
-		uint64 startTimestamp,
-		uint64 durationSeconds
-	) VestingWallet(beneficiary, startTimestamp, durationSeconds) {}
+contract MODAVestingWallet is VestingWallet, Ownable {
+	constructor(uint64 startTimestamp, uint64 durationSeconds)
+		VestingWallet(msg.sender, startTimestamp, durationSeconds)
+	{}
+
+	/**
+	 * @dev Use ownership so that beneficiary can be transferred
+	 */
+	function beneficiary() public view override returns (address) {
+		return owner();
+	}
 }
